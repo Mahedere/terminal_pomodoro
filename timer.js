f147e606a63aa9db7fa28b7d0a103add5920cfe8
Mahedere <mahederetebebe98@gmail.com>
2024-10-01 16:42:19 -0400

    Start function added

--- timer.js
+++ timer.js
@@ -1,3 +1,4 @@
+const readline = require('readline');
 function countdown(duration, type, next) {
     let remainingTime = duration;
     const interval = setInterval(() => {
@@ -17,7 +18,7 @@ function startPomodoroCycle() {
     let cycleCount = 0;
     function work() {
         console.log("Work interval started!");
-        countdown(1* 60, "Work", () => {
+        countdown(1 * 60, "Work", () => {
             cycleCount++;
             if (cycleCount % 4 === 0) {
                 longBreak();
@@ -27,14 +28,38 @@ function startPomodoroCycle() {
             }
         })
     }
-function shortBreak(){
-    console.log("Short break started!");
-    countdown(5*60,"Short Break",work);
+    function shortBreak() {
+        console.log("Short break started!");
+        countdown(5 * 60, "Short Break", work);
+    }
+    function longBreak() {
+        console.log("Long break started!");
+        countdown(15 * 60, "Long Break", work);
+    }
+    work();
 }
-function longBreak(){
-    console.log("Long break started!");
-    countdown(15*60,"Long Break",work);
+function stopTimer() {
+    clearInterval(currentInterval);
+    console.log("Timer Stopped");
 }
-work();
-}
-startPomodoroCycle(10);
+const inputHandler = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+inputHandler.on('line', (input) => {
+    switch (input) {
+        case 'start':
+            startPomodoroCycle();
+            break;
+        case 'stop':
+            stopTimer();
+            break;
+        case 'quit':
+            inputHandler.close();
+            process.exit();
+            break;
+        default:
+            console.log("Unknown Choose from:start,stop,quit")
+    }
+})
+console.log("Well hello You wanna concentrate type 'start' or 'quit' ")
