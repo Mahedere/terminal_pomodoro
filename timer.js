226602b88c1e1042ff8b7bbf1aa5c2ef5efc3b38
Mahedere <mahederetebebe98@gmail.com>
2024-10-01 11:48:33 -0400

    pomodoro cycle function added

--- timer.js
+++ timer.js
@@ -9,8 +9,32 @@ function countdown(duration, type, next) {
         if (remainingTime < 0) {
             clearInterval(interval);
             console.log(`${type} finished!`);
-            if(next)next();
+            if (next) next();
         }
     }, 1000);
 }
-countdown(10);
+function startPomodoroCycle() {
+    let cycleCount = 0;
+    function work() {
+        console.log("Work interval started!");
+        countdown(1* 60, "Work", () => {
+            cycleCount++;
+            if (cycleCount % 4 === 0) {
+                longBreak();
+            }
+            else {
+                shortBreak();
+            }
+        })
+    }
+function shortBreak(){
+    console.log("Short break started!");
+    countdown(5*60,"Short Break",work);
+}
+function longBreak(){
+    console.log("Long break started!");
+    countdown(15*60,"Long Break",work);
+}
+work();
+}
+startPomodoroCycle(10);
